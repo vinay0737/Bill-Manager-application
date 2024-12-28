2564ab46196e5f4357aacc9f3231171991426e17
vinay0737 <vekkuluri0737@gmail.com>
2024-12-28 21:36:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import "./index.css";
 
-// Get the root element and render the app
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
